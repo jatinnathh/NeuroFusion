52df6225d46a5a92d57fb810b4d6683ec323e659
Jatin Nath <146866801+jatinnathh@users.noreply.github.com>
2025-08-04 08:25:55 +0530

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -4,12 +4,10 @@ import json
 hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
 
-# fallback if Docker IP is detected
 if ip.startswith("172."):
     import netifaces as ni
-    ip = ni.ifaddresses("Wi-Fi")[ni.AF_INET][0]["addr"]  # or eth0/wlan0 as per your OS
-
+    ip = ni.ifaddresses("Wi-Fi")[ni.AF_INET][0]["addr"]  
 with open("backend/ip.json", "w") as f:
     json.dump({"ip": ip}, f)
 
-print("✅ Saved IP to backend/ip.json:", ip)
+print(" Saved IP to backend/ip.json:", ip)
